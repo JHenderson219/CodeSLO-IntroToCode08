677b84d25df15d5fa42bb34455a64507c67ec717
JHenderson219 <hendersonj219@gmail.com>
2017-04-27 21:00:10 -0700

    Complete codingEx01

---
+++ codingEx01.js
@@ -0,0 +1,19 @@
+var phoneBook = {
+    Bobby: "805-284-5688",
+    DeShaun: "707-454-8743",
+    Suzie: "805-674-3333",
+    Carmen: "510-544-9812",
+    Chow: "805-233-8754"
+};
+
+
+function forIn(obj){
+	for (key in obj){
+		return key+": "+obj[key];
+	}
+
+}
+// create a for / in loop that lists all the numbers in the 805 area code, along with who they belong to
+
+
+console.log(forIn(phoneBook));
